c3cc90fa2b7c17047641a09098ef9abe3cdd9276
emvmontiel <emvmontiel@gmail.com>
2025-10-30 16:58:26 +0800

    backend full course replayed chap 2

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -1,20 +1,20 @@
 const express = require('express'); // imports the express package
-const app = express(); // creates an express app instance
+const app = express(); // creates an express app instance; creates the backend application for us
 const PORT = 3001; // port for the server to listen on
 
 app.get('/', (req, res) => {
     console.log('I hit an endpoint!', req.method); // logs to console when the endpoint is hit
-    res.sendStatus(201); // sends an HTTP status code of 200 back to the client
+    res.status(201).send(`<a href='/dashboard'>Dashboard</a>`); // sends an HTTP status code of 201 back to the client
+    
 });
 
 app.get('/dashboard', (req, res) => {
     console.log('I hit another endpoint!'); // logs to console when the endpoint is hit
-    res.send(`Good day, what's your name?`); // sends an HTTP status code of 200 back to the client
+    res.send(`Good day, what's your name?
+        <a href='/'>Home</a>`
+    ); // sends an HTTP status code of 200 back to the client
+    
 });
 
 // Start listening on the specified port
 app.listen(PORT, () => console.log(`Server has started on: http://localhost:${PORT}`));
-
-console.log('Hello, Backend Full Course!');
-console.log('Heyyy');
-
