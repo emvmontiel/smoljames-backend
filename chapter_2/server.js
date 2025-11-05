df9c826e88c222655efb410d8143b10fb75e2f79
emvmontiel <emvmontiel@gmail.com>
2025-11-05 15:23:19 +0800

    created different endpoints

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -4,16 +4,24 @@ const PORT = 3001; // port for the server to listen on
 
 app.get('/', (req, res) => {
     console.log('I hit an endpoint!', req.method); // logs to console when the endpoint is hit
-    res.status(201).send(`<a href='/dashboard'>Dashboard</a>`); // sends an HTTP status code of 201 back to the client
-    
+    res.status(201).send(`<a href='/dashboard'>Dashboard</a>
+        <a href='/settings'>Settings</a>`); // sends an HTTP status code of 201 back to the client
 });
 
 app.get('/dashboard', (req, res) => {
     console.log('I hit another endpoint!'); // logs to console when the endpoint is hit
     res.send(`Good day, what's your name?
-        <a href='/'>Home</a>`
+        <a href='/'>Home</a>
+        <a href='/settings'>Settings</a>`
     ); // sends an HTTP status code of 200 back to the client
-    
+});
+
+app.get('/settings', (req, res) => {
+    console.log('I hit one. I am on fire today!');
+    res.send(`<h1>this is html code</h1>
+        <a href='/'>Home</a>
+        <a href='/dashboard'>Dashboard</a>`
+    );
 });
 
 // Start listening on the specified port
