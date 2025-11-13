0568936e2d360f0584891eadefad43a90bea6119
emvmontiel <emvmontiel@gmail.com>
2025-11-13 17:57:19 +0800

    utilize express.Router()

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import path, { dirname } from 'path'
+import express from 'express';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express()
 const PORT = process.env.PORT || 5000; // Checks if there is a port envirnment variable; if there isn't, it defaults to 5000
@@ -12,10 +13,16 @@ const __dirname = dirname(__filename) // Gets directory name from the file path
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public'))) // Locates public directory; configure Express to serve static assets located in the '../public' folder
 
+// Retrieves HTML file from the /public directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 }) 
 
+// Instead of writing down here all of the endpoints, we can just invoke the following.
+// For any route with the /auth path, we just use authRoutes
+// We need to import authRoutes because it is another file in a different location
+// Takes all the routes defined in the authRoutes and concatenates it on the end of the /auth path 
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
