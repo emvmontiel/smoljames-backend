31c13a4cc8f04fd562f74905a77e344ab11e6cec
emvmontiel <emvmontiel@gmail.com>
2025-11-27 17:14:15 +0800

    finished setting up update & delete endpoints

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -31,9 +31,26 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     const { completed } = req.body
     const { id } = req.params
+    const { page } = req.query
+
+    const updatedTodo = db.prepare (`
+        UPDATE todos 
+        SET completed = ? 
+        WHERE id = ?`)
+    
+    updatedTodo.run(completed, id)
+    res.json({ message: "Todo completed "})
 });
 
 // Delete
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare (`
+        DELETE FROM todos 
+        WHERE id = ?
+        AND user_id = ?`)
+    deleteTodo.run(id, userId)
+});
 
 export default router
